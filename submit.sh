42a64fc0d5f8fde467c9cede51cb38d908d2af18
objektwerks <objektwerks@runbox.com>
2019-12-01 09:02:39 -0500

    edited app name in submit.sh

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 spark-submit \
-  --class spark.JdbcHiveApp \
+  --class spark.JdbcHiveJob \
   --master local[2] \
   --packages com.typesafe:config:1.3.4, com.oracle.ojdbc:ojdbc8:19.3.0.0 \
   ./target/scala-2.11/spark-jdbc-hive_2.11-0.1-SNAPSHOT.jar
